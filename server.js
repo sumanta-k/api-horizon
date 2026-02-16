97e70dd0b1e871fb63d5393890e736dc855565b2
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-02-16 20:25:25 +0530

    fixes #6 : check before serving data to user

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import { createServer } from "node:http";
 const PORT = 3000;
 
 const server = createServer((req, res) => {
-    if (req.url === "/api") {
+    if (req.url === "/api" && req.method === "GET") {
         res.end("data has been sent from /api route");
     }
 });
