d2caa7655f3d57e6304c247a76b00f28fa224b4c
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-02-16 20:12:13 +0530

    check how many times request has been reached to server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { createServer } from "node:http";
 const PORT = 3000;
 
 const server = createServer((req, res) => {
+    console.log(req.headers);
     if (req.url === "/api") {
         res.end("data has been sent from /api route");
     }
